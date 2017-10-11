2503b7a8eb0ea67255c09caf2acab8a35e083190
momarcissex <momar@nxtdrop.com>
2017-10-10 23:02:08 -0400

    Added form

--- login.php
+++ login.php
@@ -10,6 +10,16 @@
     </head>
 
     <body>
-        
+        <form>
+            <p>Username: </p><input type="text" name="username" placeholder="Username">
+            <p>Password: </p><input type="password" name="pwd" placeholder="Password">
+            <p>Don't have an account?</p>
+            <p>First Name: </p><input type="text" name="fname" placeholder="First Name">
+            <p>Last Name: </p><input type="text" name="lname" placeholder="Last Name">
+            <p>Email: </p><input type="text" name="email" placeholder="Email">
+            <p>Username: </p><input type="text" name="username" placeholder="Username">
+            <p>Password: </p><input type="password" name="pwd" placeholder="Password">
+            <p>Confirm Password: </p><input type="password" name="cpwd" placeholder="Confirm Password">
+        </form>
     </body>
 </html>
