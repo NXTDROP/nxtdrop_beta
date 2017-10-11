c5b38f2efc43f7d9ccb87a92223d6ca3ff4a3ce9
momarcissex <momar@nxtdrop.com>
2017-10-10 22:47:35 -0400

    Login page added

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <title>
+        NXTDROP: The Social Marketplace
+    </title>
+    <head>
+        <link type="text/css" rel="stylesheet" href="login.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    </head>
+
+    <body>
+        
+    </body>
+</html>
