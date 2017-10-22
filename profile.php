c60beeb3e5bf2fb4180acb5bbfc1c573b36ba16b
momarcissex <momar@nxtdrop.com>
2017-10-22 01:22:15 -0400

    added link to edit profile

--- profile.php
+++ profile.php
@@ -47,7 +47,7 @@
             <h2>Momar Cisse</h2>
             <h3>@momarcissex</h3>
             <p>God'Speed™ 👕 • they never loved us.</p>
-            <button class="edit-button">Edit Profile</button>
+            <a href="edit-profile.php"><button class="edit-button">Edit Profile</button></a>
         </div>
 
         <section class="container">
