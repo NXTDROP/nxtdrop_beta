18f0cfe119d86a84ab539877091824727aecc502
momarcissex <momar@nxtdrop.com>
2017-10-22 02:47:51 -0400

    Edit profile Designed done

--- edit-profile.php
+++ edit-profile.php
@@ -10,10 +10,9 @@
         NXTDROP: The Social Marketplace
     </title>
     <head>
-        <link type="text/css" rel="stylesheet" href="login.css" />
+        <link type="text/css" rel="stylesheet" href="edit-profile.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="regis/registration.js"></script>
     </head>
 
     <body>
@@ -22,15 +21,23 @@
         </header>
         
         <div class="container">
-            <form action="login/login.php" method="POST" class="login-form">
-                <input type="text" name="username" placeholder="Name" required></br>
+            <form action="" method="POST" class="login-form">
+                <input type="text" name="first_name" placeholder="First Name" required></br>
+                <input type="text" name="last_name" placeholder="Last Name" required></br>
+                <input type="text" name="email" placeholder="Email" required></br>
                 <input type="password" name="pwd" placeholder="@Username" required></br>
-            </form>
-            <div><?php include('login/error.php'); ?></div>
-            <form action="" method="POST" id="signup" class="signup-form">
-                <textarea></textarea>
+                <textarea placeholder="Bio"></textarea></br>
                 <button type="submit" name="submit" id="submit">Save Changes</button>
             </form>
+</br></br>
+            <form action="" method="POST" class="">
+            <input type="password" name="pwd" placeholder="Enter New Password" required></br>
+            <input type="password" name="cpwd" placeholder="Confirm Password" required></br>
+            <button type="submit" name="submit" id="submit">Change Password</button>
+            </form>
+
+</br></br>
+            <a href="profile.php"><p>Back to Profile</p></a>
         </div>
     </body>
 </html>
