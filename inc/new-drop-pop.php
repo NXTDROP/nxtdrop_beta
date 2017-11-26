2a7e77ffe2c735978bf93153af7e840193fd2dca
momarcissex <momar@nxtdrop.com>
2017-11-25 23:47:58 -0500

    Cut & Paste drop pop on new php file

---
+++ inc/new-drop-pop.php
@@ -0,0 +1,14 @@
+<div class="post">
+            <div class="post_close close"></div>
+            <div class="post_main">
+                <h2>New Drop</h2>
+                <div class="post_content">
+                    <form action="post/post.php" method="POST" enctype="multipart/form-data" id="post" class="post-form">
+                        <textarea name="caption" placeholder="Enter Description" id="caption"></textarea>
+                        <input type="file" name="file" id="file" class="inputfile" accept="image/*" data-multiple-caption="{count} files selected" multiple />
+                        <label for="file"><i class="fa fa-picture-o" aria-hidden="true"></i></label>
+                        <button type="submit" name="submit" id="submit">Drop</button>
+                    </form>
+                </div>
+            </div>
+</div>
--- index.php
+++ index.php
@@ -21,20 +21,7 @@
 
         <p id="message"></p>
 
-        <div class="post">
-            <div class="post_close close"></div>
-            <div class="post_main">
-                <h2>New Drop</h2>
-                <div class="post_content">
-                    <form action="post/post.php" method="POST" enctype="multipart/form-data" id="post" class="post-form">
-                        <textarea name="caption" placeholder="Enter Description" id="caption"></textarea>
-                        <input type="file" name="file" id="file" class="inputfile" accept="image/*" data-multiple-caption="{count} files selected" multiple />
-                        <label for="file"><i class="fa fa-picture-o" aria-hidden="true"></i></label>
-                        <button type="submit" name="submit" id="submit">Drop</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+        <?php include('inc/new-drop-pop.php'); ?>
 
         <section class="footer">
             <ul>
