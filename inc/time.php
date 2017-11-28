549adadfdddec3aedf97c0a26e9e6c5fc56d524a
momarcissex <momar@nxtdrop.com>
2017-11-28 02:25:05 -0500

    Time display bug fixed

--- inc/time.php
+++ inc/time.php
@@ -12,24 +12,34 @@
         $date->setTimeZone(new DateTimeZone($json_data['timezone']));
         $date2 = new DateTime($data);
         $date2->setTimeZone(new DateTimeZone($json_data['timezone']));
-        $date = $date->format('Y-m-d H:i:s') . "\n";
-        $date2 = $date2->format('Y-m-d H:i:s') . "\n";
-        $diff = abs(strtotime($date2) - strtotime($date));
+        $new_date = $date->format('Y-m-d H:i:s') . "\n";
+        $new_date2 = $date2->format('Y-m-d H:i:s') . "\n";
+        $diff = strtotime($new_date) - strtotime($new_date2);
         
-        if ($diff < 60) {
-            return $diff . " s";
-        }
-        else if ($diff < 3600) {
-            return floor($diff/60) . " m";
-        }
-        else if ($diff/(3600) < 24) {
-            return floor($diff/3600) . " h";
-        }
-        else if (floor($diff/86400)  < 365) {
-            return floor($diff/86400) . " d";
+        if ($diff > 0) {
+            if ($diff > 0 && $diff < 60) {
+                return $diff . " s";
+            }
+            else if ($diff < 3600) {
+                return floor($diff/60) . " m";
+            }
+            else if ($diff/(3600) < 24) {
+                return floor($diff/3600) . " h";
+            }
+            else if (floor($diff/86400)  < 7) {
+                return floor($diff/86400) . " d";
+            }
+            else {
+                if (intval($new_date) == intval($new_date2)) {
+                    return $date2->format('M j');
+                }
+                else {
+                    return $date2->format('j M Y');
+                }        
+            }
         }
         else {
-            return floor(($diff/86400) / 365) . " y";
+            return "0 s";
         }
 
     }
