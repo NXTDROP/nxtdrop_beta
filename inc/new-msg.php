7636f01a72d30e2738cda1a1fa2d4b89ed957c3d
momarcissex <momar@nxtdrop.com>
2017-12-21 13:06:25 -0500

    Fixed bug in message page

--- inc/new-msg.php
+++ inc/new-msg.php
@@ -3,16 +3,13 @@
     <div class="msg_main">
         <h2>New Message</h2>
         <div class="msg_content">
-        <p>Send Message To:</p>
-        <div class="enter-name"></div>
-        <form action="new-search.php" method="GET" id="new-search-bar">
-        <input type="text" name="q" size="60" placeholder="Enter Name"/>
-        <textarea class="new-msg_input" placeholder="Enter Message..."></textarea>
-        <button class="send_button">Send</button>
+            <p>Send Message To:</p>
+            <div class="enter-name"></div>
+            <form action="" method="GET" id="new-search-bar">
+                <input type="text" name="q" size="60" placeholder="Enter Name"/>
+                <textarea class="new-msg_input" placeholder="Enter Message..."></textarea>
+                <button type="submit" name="send" id="send">Send</button>
+            </form>
         </div>
-        </div>
-        
-    </div>
-        
-        
+    </div>       
 </div>
