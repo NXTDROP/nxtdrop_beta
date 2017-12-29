d25b7040d46aa8fc99a6baf17953f5a248cbe862
youssoupha24 <32562208+youssoupha24@users.noreply.github.com>
2017-12-29 04:13:54 -0500

    message badge icon update

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <script type="text/javascript" src="js/post-popup.js"></script>
         <script type="text/javascript" src="js/delete-post.js"></script>
         <script type="text/javascript" src="js/like-unlike-post.js"></script>
+        <script type="text/javascript" src="js/dm_icon.js"></script>
     </head>
     <body>
         <?php include('inc/header-body.php'); ?>
---
+++ js/dm_icon.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.dm_icon').click(function() {
+        $('.dm_icon').html('<span class="fa-stack has-badge" data-count=""><i class="fa fa-envelope" aria-hidden="true" title="Direct Messages"></i></span>');
+    });
+});
