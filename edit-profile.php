5dc167a4fbc97913f9988612147f7a2296667d96
Momar Cisse <momar@nxtdrop.com>
2018-09-06 02:05:28 -0400

    SD issue fixed

--- edit-profile.php
+++ edit-profile.php
@@ -142,7 +142,6 @@
                 });
             });
         </script>
-        </script>
         <!-- Facebook Pixel Code -->
         <script>
         !function(f,b,e,v,n,t,s)
@@ -160,6 +159,27 @@
     </head>
 
     <body>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                appId      : '{your-app-id}',
+                cookie     : true,
+                xfbml      : true,
+                version    : '{api-version}'
+                });
+                
+                FB.AppEvents.logPageView();   
+                
+            };
+
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
         <header>
             <a href="home"><img id ="logo"src="img/nxtdroplogo.png" width="125px"></a>
         </header>
--- welcome_home.php
+++ welcome_home.php
@@ -25,82 +25,103 @@
     </head>
 
     <body>
-    <script>
-        $(document).ready(function() {
-            var x;
-            $('#search_index').focusin(function() {
-                $('.search_results_index').show();
-                $('#home-carousel').hide("slow");
-            });
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                appId      : '{your-app-id}',
+                cookie     : true,
+                xfbml      : true,
+                version    : '{api-version}'
+                });
+                
+                FB.AppEvents.logPageView();   
+                
+            };
 
-            $('#search_index').focusout(function() {
-                var search = $(this).val();
-
-                if (search != '') {
-                    x = setTimeout(function() {
-                        $('#home-carousel').show("slow");
-                        $('.search_results_index').hide();
-                    }, 300000);
-                }
-                else {
-                    x = setTimeout(function() {
-                        $('#home-carousel').show("slow");
-                        $('.search_results_index').hide();
-                    }, 2500);
-                }
-            });
-
-            $('#search_index').keyup(function() {
-                var search = $(this).val();
-
-                if (search != '') {
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <script>
+            $(document).ready(function() {
+                var x;
+                $('#search_index').focusin(function() {
+                    $('.search_results_index').show();
                     $('#home-carousel').hide("slow");
-                }
+                });
 
-                $.ajax({
-                    url: 'inc/search_users.php',
-                    type: 'GET',
-                    data: {search: search},
-                    dataType: 'text',
-                    success: function(data) {
-                        $('.search_results_index').html(data);
+                $('#search_index').focusout(function() {
+                    var search = $(this).val();
+
+                    if (search != '') {
+                        x = setTimeout(function() {
+                            $('#home-carousel').show("slow");
+                            $('.search_results_index').hide();
+                        }, 300000);
+                    }
+                    else {
+                        x = setTimeout(function() {
+                            $('#home-carousel').show("slow");
+                            $('.search_results_index').hide();
+                        }, 2500);
+                    }
+                });
+
+                $('#search_index').keyup(function() {
+                    var search = $(this).val();
+
+                    if (search != '') {
+                        $('#home-carousel').hide("slow");
+                    }
+
+                    $.ajax({
+                        url: 'inc/search_users.php',
+                        type: 'GET',
+                        data: {search: search},
+                        dataType: 'text',
+                        success: function(data) {
+                            $('.search_results_index').html(data);
+                        }
+                    });
+                });
+
+                $(document).scroll(function() {
+                    if ($(document).scrollTop() >= 60) {
+                        $('.navbar').css('background', '#fff');
+                        $('.navbar').css('border-bottom', '1px solid #e6e1e1');
+                    }
+                    else {
+                        $('.navbar').css('background', 'transparent');
+                        $('.navbar').css('border-bottom', 'none');
                     }
                 });
             });
+        </script>
 
-            $(document).scroll(function() {
-                if ($(document).scrollTop() >= 60) {
-                    $('.navbar').css('background', '#fff');
-                    $('.navbar').css('border-bottom', '1px solid #e6e1e1');
-                }
-                else {
-                    $('.navbar').css('background', 'transparent');
-                    $('.navbar').css('border-bottom', 'none');
-                }
-            });
-        });
-    </script>
+        <nav class="navbar navbar-expand-xl nav_home">
+            <a href="https://nxtdrop.com" id="navbar-brand"><img src="https://nxtdrop.com/img/nxtdroplogo.png"></a>
+            <a id="login-btn" href="login_signup"><button class="sign-btn" title="Login/Sign Up">SIGN IN</button></a>
+        </nav>
 
-    <nav class="navbar navbar-expand-xl nav_home">
-        <a href="https://nxtdrop.com" id="navbar-brand"><img src="https://nxtdrop.com/img/nxtdroplogo.png"></a>
-        <a id="login-btn" href="login_signup"><button class="sign-btn" title="Login/Sign Up">SIGN IN</button></a>
-    </nav>
-
-    <div id="home-carousel" class="carousel slide carousel-fade" data-ride="carousel">
-        <div class="carousel-inner">
-            <!--<div class="carousel-item active">
-            <img class="d-block w-100" src="img/AdAug2018(2).png" alt="All shoes are verified authentic.">
-            </div>-->
+        <div id="home-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+                <!--<div class="carousel-item active">
+                <img class="d-block w-100" src="img/AdAug2018(2).png" alt="All shoes are verified authentic.">
+                </div>-->
+            </div>
         </div>
-    </div>
 
-    <div class="search_bar_index">
-        <input type="search" id="search_index" placeholder="What are you looking for?"/>
-    </div>
+        <div class="search_bar_index">
+            <input type="search" id="search_index" placeholder="What are you looking for?"/>
+        </div>
 
-    <div class="search_results_index">
-        
-    </div>
+        <div class="search_results_index">
+            
+        </div>
 
     </body>
 </html>
